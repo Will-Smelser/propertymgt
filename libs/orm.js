386f487dd51e5363fb580f1337df91b24b77995e
Will-Smelser <willsmelser@gmail.com>
2015-01-05 18:53:36 -0500

    Got the functionality needed and should be more extensible.

--- libs/orm.js
+++ libs/orm.js
@@ -2,65 +2,65 @@
 
 var Q = require("q");
 
-var Query = function(nanoDb, schema){
+var Query = function (nanoDb, schema) {
 
-    this._getView =  function(){
+    this._getView = function () {
         var deferred = Q.defer();
-        nanoDb.get("_design/"+schema.getName(),function(err,body){
-            if(err){
+        nanoDb.get("_design/" + schema.getName(), function (err, body) {
+            if (err) {
                 deferred.reject(err);
-            }else{
+            } else {
                 deferred.resolve(body);
             }
         });
         return deferred.promise;
     };
 
-    this._createView = function(schema,type){
+    this._createView = function (schema, type) {
         var deferred = Q.defer();
 
         //create the syntax for creating a view
         var result = {
-            '_id'      : "_design/"+schema.getName(),
-            'language' : "javascript",
-            'views'    : {}
+            '_id': "_design/" + schema.getName(),
+            'language': "javascript",
+            'views': {}
         };
-        result.views["by"+type.id] = {
-            map : "function(doc){\n\tif(doc.name==='"+schema.getName()+"'){\n\t\tfor(var x in doc.data){\n\t\t\tif(doc.data[x].id==='"+type.id+"') emit(doc.data[x].value,doc);\n\t\t}\n\t}\n}"
+        result.views["by" + type.id] = {
+            map: "function(doc){\n\tif(doc.name==='" + schema.getName() + "'){\n\t\tfor(var x in doc.data){\n\t\t\tif(doc.data[x].id==='" + type.id + "') emit(doc.data[x].value,doc);\n\t\t}\n\t}\n}"
         };
 
         //lookup the current views
         this._getView()
-            .then(function(body){
+            .then(function (body) {
                 //the schema.name existed, so the view might already exist
-                body.views["by"+type.id] = result.views["by"+type.id];
+                body.views["by" + type.id] = result.views["by" + type.id];
 
-                nanoDb.insert(body,null,function(err,body2){
-                    if(err)
+                nanoDb.insert(body, null, function (err, body2) {
+                    if (err)
                         deferred.reject(err);
                     else
                         deferred.resolve(body2);
                 });
             })
-            .fail(function(err){
-                if(err.error === "not_found"){
-                    nanoDb.insert(result,null,function(err2,body){
-                        if(err2)
+            .fail(function (err) {
+                if (err.error === "not_found") {
+                    nanoDb.insert(result, null, function (err2, body) {
+                        if (err2)
                             deferred.reject(err2);
                         else
                             deferred.resolve(body);
                     });
-                }else
+                } else
                     deferred.reject(err);
             });
         return deferred.promise;
     }
 
-    this.insert = function(schemaObj){
+    this.insert = function (schemaObj) {
         var deferred = Q.defer();
         var serialized = schemaObj.serialize();
-        nanoDb.insert(serialized,null,function(err,body){
-            if(err)
+        nanoDb.insert(serialized, null, function (err, body) {
+            if (err)
                 deferred.reject(err);
             else
                 deferred.resolve(body);
@@ -69,34 +69,35 @@ var Query = function(nanoDb, schema){
     }
 
     this.find = {
-        by : {}
+        by: {}
     }
 
     var self = this;
-    this.find.register = function(type){
+    this.find.register = function (field) {
+        var type = field.type;
         var deferred = Q.defer();
         var ready = false;
-        self._createView(schema,type)
-            .then(function(body){
+        self._createView(schema, type)
+            .then(function (body) {
                 ready = true;
                 deferred.resolve();
             })
-            .fail(function(err){
+            .fail(function (err) {
                 deferred.reject(err);
             });
 
         //even if things failed we register this function
-        self.find.by[type.id] = function(value){
+        self.find.by[type.id] = function (value) {
             var deferred2 = Q.defer();
-            if(!ready) console.log("Query.find.by."+type.id+" before ready.");
+            if (!ready) console.log("Query.find.by." + type.id + " before ready.");
 
-            nanoDb.view(schema.getName(),'by'+type.id,{key:value},function(err,body2){
-                if(err)
+            nanoDb.view(schema.getName(), 'by' + type.id, {key: value}, function (err, body2) {
+                if (err)
                     deferred2.reject(err);
-                else{
+                else {
                     var result = [];
-                    for(var x in body2.rows){
-                        result.push(ORM.create(schema,body2.rows[x].value));
+                    for (var x in body2.rows) {
+                        result.push(ORM.create(schema, body2.rows[x].value));
                     }
                     deferred2.resolve(result);
                 }
@@ -110,126 +111,193 @@ var Query = function(nanoDb, schema){
 };
 
 var ORM = {
-        "Query" : Query,
-        deserialize : function(serialized){
-            var type = new this.types[serialized.type.type](null,null,null,null);
-            type.deserialize(serialized.type);
+    "Query": Query,
+    /**
+     * Create a Field object from a serialized field
+     * @param serialized
+     * @returns {ORM.Field}
+     */
+    deserialize: function (serialized) {
+        var type = null;
+        var others = [];
+        for (var x in serialized) {
 
-            var view = new this.views[serialized.view.type](null,null,null,null);
-            view.deserialize(serialized.view);
+            var temp = new ORM[x][serialized[x].type](null, null, null, null);
+            temp.deserialize(serialized[x]);
 
-            return new this.Field(type,view);
-        },
-        Field : function(type,view){
-            this.type = type;
-            this.view = view;
-
-            this.serialize = function(){
-                return {
-                    type : this.type.serialize(),
-                    view : this.view.serialize()
-                };
-            };
-
-            return this;
-        },
-        types : {
-            String : function(id,value){
-                if(typeof value === "undefined")
-                    value = null;
-
-                this.type = "String";
-                this.value= value;
-                this.id   = id;
-                var self = this;
-
-                this.serialize = function(){
-                    return {type:"String",value:self.value,id:self.id};
-                };
-
-                this.deserialize = function(serialized){
-                    self.value = serialized.value;
-                    self.id    = serialized.id;
-                };
-                return this;
-            }
-        },
-        views : {
-            Input : function(cssClassName){
-                this.type = "Input";
-                this.name = cssClassName;
-                var self = this;
-
-                this.serialize = function(){
-                    return {type:"Input",name:self.name};
-                };
-
-                this.deserialize = function(serialized){
-                    self.name = serialized.name;
-                };
-                this.dom = function(){
-                    return document.createElement("input").setAttribute("class", cssClassName);
-                }
-                return this;
-            }
-        },
-        create : function(schema, data){
-            var self = this;
-            var newSchema = new this.Schema(data.name);
-            schema.each(function(idx,field){
-                var type = new self.types[field.type.type](null,null,null,null);
-
-                //TODO: this needs improved
-                //have to find the matching id, first we assume indexes are same
-                if(field.type.id === null ||
-                    (data.data[idx] && data.data[idx] !== null && data.data[idx].id === field.type.id)){
-                    type.deserialize(data.data[idx]);
-                }else{
-                    for(var x in data.data){
-                        if(data.data[x].id === field.type.id )
-                            type.desialize(data.data[x]);
-                    }
-                }
-
-                var newField = new self.Field(type,field.view);
-                newSchema.add(newField);
-
-            });
-
-            if(typeof data._id !== "undefined")
-                newSchema._id = data._id;
-            if(typeof data._rev !== "undefined")
-                newSchema._rev = data._rev;
-
-            return newSchema;
-        },
-        Schema : function(name){
-            var self = this;
-            this.name = name;
-            this.fields = [];
-            return {
-                getName : function(){
-                    return self.name;
-                },
-                add : function(field){
-                    self.fields.push(field);
-                },
-                serialize : function(){
-                    var result = [];
-                    for(var x in self.fields){
-                        result.push(self.fields[x].type.serialize())
-                    }
-                    return {
-                        name:self.name,
-                        data:result
-                    };
-                },
-                each : function(func){
-                    for(var x in self.fields)
-                        func(x,self.fields[x]);
-                }
-            };
+            if (x === "types")
+                type = temp;
+            else
+                others.push(temp);
         }
-    };
+        return new ORM.Field(type, others);
+    },
+    Field: function (type, others) {
+        if (typeof others !== "object" || typeof others.length === "undefined")
+            others = [];
+
+        this.type = type;
+        this.others = others;
+
+        var self = this;
+        this.serialize = function () {
+            var result = {};
+            result[self.type._class] = this.type.serialize();
+
+            for (var x in self.others)
+                result[self.others[x]._class] = self.others[x].serialize();
+
+            return result;
+        };
+
+        return this;
+    },
+    types: {
+        String: function (id, args) {
+            var value = null;
+            if (args !== null && typeof args !== "undefined" && typeof args.length != "undefined" && args.length > 0)
+                value = args[0];
+
+            this._class = "types";
+            this.type = "String";
+            this.value = value;
+            this.id = id;
+            var self = this;
+
+            this.serialize = function () {
+                return {type: "String", value: self.value, id: self.id};
+            };
+
+            this.deserialize = function (serialized) {
+                self.value = serialized.value;
+                self.id = serialized.id;
+            };
+            return this;
+        }
+    },
+    views: {
+        Input: function (cssClassName, attributes) {
+            this._class = "views";
+            this.type = "Input";
+            this.name = cssClassName;
+            var self = this;
+
+            this.serialize = function () {
+                return {type: "Input", name: self.name};
+            };
+
+            this.deserialize = function (serialized) {
+                self.name = serialized.name;
+            };
+            this.dom = function () {
+                return document.createElement("input").setAttribute("class", cssClassName);
+            }
+            this.toString = function () {
+                console.log(attributes);
+                var result = '<input type="text" class="' + cssClassName + '" ';
+                for (var x in attributes) {
+                    result += x + '="' + attributes[x] + '" ';
+                }
+                return result + " />";
+            }
+            return this;
+        }
+    },
+    /**
+     * Clones a schema and then takes the json data
+     * and updates the schema values with the data.
+     * @param schema
+     * @param data
+     * @returns {*|Node|Tag|Block}
+     */
+    create: function (schema, data) {
+
+        var newSchema = schema.clone();
+
+        for(var x in data.data){
+            var type = new ORM.types[data.data[x].type](data.data[x].id);
+            type.deserialize(data.data[x])
+            newSchema.setType(type);
+        }
+
+        return newSchema;
+    },
+    Schema: function (name, config) {
+        var self = this;
+        this.name = name;
+        this.fields = [];
+
+        //fill the fields from a config object
+        if (typeof config === "object") {
+            for (var name in config) {
+                var type = null;
+                var others = [];
+
+                for(var x in config[name]){
+                    var args = config[name][x].slice(1);
+                    var obj = config[name][x][0];
+
+                    if(x === "type"){
+                        type = new obj(name,args);
+                    }else{
+                        others.push(new obj(name,args));
+                    }
+                }
+
+                self.fields.push(new ORM.Field(type, others));
+            }
+        }
+
+        return {
+            setType : function(type){
+                var temp = null;
+
+                var x = null;
+                for (x in self.fields)
+                    if (self.fields[x].type.id === type.id)
+                        break;
+
+                if(x === null) console.log("setType failed.  Could not find type ("+type.id+") in Schema.")
+
+                self.fields[x].type = type;
+            },
+            getName: function () {
+                return self.name;
+            },
+            getField: function (typeId) {
+                for (var x in self.fields) {
+                    if (self.fields[x].type.id === typeId) {
+                        return self.fields[x];
+                    }
+                }
+            },
+            add: function (field) {
+                self.fields.push(field);
+            },
+            serialize: function () {
+                var result = [];
+                for (var x in self.fields) {
+                    result.push(self.fields[x].type.serialize())
+                }
+                return {
+                    name: self.name,
+                    data: result
+                };
+            },
+            clone : function(){
+                var name = self.name;
+                var schema = new ORM.Schema(name);
+                for(var x in self.fields){
+                    schema.add(ORM.deserialize(self.fields[x].serialize()));
+                }
+                return schema;
+            },
+            each: function (func) {
+                for (var x in self.fields)
+                    func(x, self.fields[x]);
+            }
+        };
+    }
+};
 
 module.exports = ORM;
--- models/property.js
+++ models/property.js
@@ -3,7 +3,7 @@
 var nano    = require('nano')('http://localhost:5984');
 var db      = nano.use("propmgt");
 var orm     = require('../libs/orm');
-
+/*
 //promise library
 var qPromise = require("q");
 
@@ -16,13 +16,14 @@ schema.add(new orm.Field(new orm.types.String("PropAddrLine1","203 Clinton St.")
 schema.add(new orm.Field(new orm.types.String("PropAddrLine2",""),new orm.views.Input("PropAddrLine2")));
 
 var query = new orm.Query(db,schema);
-
+*/
 /*
 query.insert(schema)
     .fail(function(err){console.log("Insert Failed",err);})
     .then(function(body){
         console.log("Insert Success",body);
     });*/
+/*
 query.find.register(propNameType)
     .fail(function(err){console.log("Register Failed",err);})
     .then(function(){
@@ -34,14 +35,56 @@ query.find.register(propNameType)
                     console.log(schemas[x].serialize());
             });
     });
+*/
 
+var model = {
+    PropName        : {type:[orm.types.String,"Test Property"], view:[orm.views.Input]},
+    PropAddrLine1   : {type:[orm.types.String,"Some line 1"], view:[orm.views.Input,{pattern:"[\w]+"}]},
+    PropAddrLine2   : {type:[orm.types.String,"Some line 2"], view:[orm.views.Input]},
+    PropCity        : {type:[orm.types.String,"City"], view:[orm.views.Input]},
+    PropState       : {type:[orm.types.String,"State"], view:[orm.views.Input]},
+    PropZip         : {type:[orm.types.String,"Zip"], view:[orm.views.Input]}
+};
 
-var myModel = {
-    name : {
+var schema = new orm.Schema("Property",model);
+var query = new orm.Query(db,schema);
+//query.find.register(schema.getField("PropName"));
 
-    }
-}
+var obj = schema.clone();
+console.log(schema.serialize());
 
+obj.setType(new orm.types.String("PropName",["Update Name"]));
+console.log(obj.serialize());
+
+var test = { name: 'Property', data:[
+    { type: 'String', value: 'line1', id: 'PropName' },
+    { type: 'String', value: 'line2', id: 'PropAddrLine1' },
+    //{ type: 'String', value: 'line3', id: 'PropAddrLine2' },
+    { type: 'String', value: 'line4', id: 'PropCity' },
+    { type: 'String', value: '', id: 'PropState' },
+    { type: 'String', value: 'Zip', id: 'PropZip' }
+]};
+var schema2 = orm.create(schema,test);
+console.log(schema2.serialize());
+
+/*
+query.find.by.PropName("Home")
+    .then(function(schemas){
+        for(var x in schemas)
+            console.log(schemas[x].serialize());
+    });
+*/
+//console.log(schema.serialize());
+
+//console.log(schema.getField("PropAddrLine1").view.toString());
+//console.log(schema.serialize());
+
+/*
+var field = new orm.Field(new orm.types.String("PropAddrLine1","203 Clinton St."),[new orm.views.Input("PropAddrLine1")]);
+console.log(field.serialize());
+var field2 = new orm.deserialize(field.serialize());
+console.log(field2.serialize());
+*/
 var PropertyModel = {
     model : {
         model:"PROPERTY",

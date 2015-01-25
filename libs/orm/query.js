39311c7b2c2883a63d6c2ac88f4f341264524694
Will-Smelser <willsmelser@gmail.com>
2015-01-25 14:33:29 -0500

    Fixed some errors and statuses

--- libs/orm/query.js
+++ libs/orm/query.js
@@ -2,7 +2,9 @@
 
 var Q = require("q");
 
-var Query = function (nanoDb, schema) {
+var Query = function (nanoDb, schema, ORM) {
+
+    var self = this;
 
     this._getView = function () {
         var deferred = Q.defer();
@@ -69,9 +71,10 @@ var Query = function (nanoDb, schema) {
         var serialized = schemaObj.serialize();
 
         nanoDb.insert(serialized, null, function (err, body) {
-            if (err)
-                deferred.reject(err);
-            else{
+            if (err){
+                self._log('Failed','insert',serialized);
+                deferred.reject("Query failed.<br/>"+err.name+": "+err.error.replace('_',' '));
+            }else{
                 schemaObj._rev = body.rev;
                 deferred.resolve(body);
             }
@@ -79,11 +82,35 @@ var Query = function (nanoDb, schema) {
         return deferred.promise;
     }
 
+    this.delete = function (id,revision) {
+        var deferred = Q.defer();
+        nanoDb.destroy(id,revision,function(err,body){
+            if (err){
+                self._log('Failed','delete',id,revision);
+                deferred.reject("Query failed.<br/>"+err.name+": "+err.error.replace('_',' '));
+            }else{
+                deferred.resolve(body);
+            }
+        });
+        return deferred.promise;
+    };
+
+    this._log = function(msg,method,arg1,arg2,argN){
+        var args = Array.prototype.slice.call(arguments);
+        var msg  = args.shift();
+        var func = args.shift();
+        var str = msg + '.  Query.'+func+'(';
+        var comma = '';
+        for(var x in args){
+            str +=  comma + args[x];
+            comma = ',';
+        }
+        console.log(str+')');
+    }
+
     this.find = {
         by: {}
     }
-
-    var self = this;
 
     /**
      * Register a search view.  This will take the field and call field.getType() and
@@ -104,6 +131,7 @@ var Query = function (nanoDb, schema) {
                 deferred.resolve();
             })
             .fail(function (err) {
+                self._log('Failed','find.register',field);
                 deferred.reject(err);
             });
 
@@ -115,28 +143,59 @@ var Query = function (nanoDb, schema) {
          */
         self.find.by[field.id] = function () {
             var args = Array.prototype.slice.call(arguments);
-            var value = ORM._construct(ORM.types[field.getType().type],args).serialize().value;
 
+            var value = ORM._construct(ORM.types[field.getType().type],args).getValue();
 
             var deferred2 = Q.defer();
             if (!ready) console.log("Query.find.by." + field.id + " before ready.");
 
             nanoDb.view(schema.getName(), 'by' + field.id, {key: value}, function (err, body2) {
-                if (err)
-                    deferred2.reject(err);
-                else {
+                if (err){
+                    deferred2.reject("Query failed.<br/>"+err.name+": "+err.error.replace('_',' '));
+                }else {
                     var result = [];
                     for (var x in body2.rows) {
-                        var doc = schema.clone().deserialize(body2.rows[x].value);
-                        doc._rev = body2.rows[x].value._rev;
-                        doc._id  = body2.rows[x].value._id;
-                        result.push(doc);
+                        if(schema.getName() !== body2.rows[x].name)
+                            return deferred.reject('Requested object did not match schema('+schema.getName()
+                                +' vs. '+body2.rows[x].name+').  Aborted.');
+
+                        try{
+                            var doc = schema.clone().deserialize(body2.rows[x].value);
+                            doc._rev = body2.rows[x].value._rev;
+                            doc._id  = body2.rows[x].value._id;
+                            result.push(doc);
+                        }catch(err){
+                            return deferred.reject("Failed to construct object from database.<br/>"
+                                +JSON.stringify(body2)+"<br/>"+err);
+                        }
                     }
                     deferred2.resolve(result);
                 }
             });
             return deferred2.promise;
         }
+        return deferred.promise;
+    }
+
+    self.find.by._id = function(id){
+        var deferred = Q.defer();
+        nanoDb.get(id,function(err,body){
+            if(err){
+                deferred.reject("Query failed.<br/>"+err.name+": "+err.error.replace('_',' '));
+            }else if(schema.getName() !== body.name){
+                deferred.reject('Requested object did not match schema ('+schema.getName()
+                    +' vs. '+body.name+' ).  Aborted.');
+            }else{
+                var doc = schema.clone();
+                try{
+                     doc = doc.deserialize(body);
+                }catch(err){
+                    return deferred.reject("Failed to construct object from database.<br/>"
+                        +JSON.stringify(body)+"<br/>"+err);
+                }
+                deferred.resolve(doc);
+            }
+        });
         return deferred.promise;
     }
 
--- libs/orm/view.js
+++ libs/orm/view.js
@@ -1,37 +1,140 @@
 'use strict'
 
+var Q = require("q");
 var fs = require('fs');
 var Handlebars = require('handlebars');
 
-Handlebars.registerHelper('list', function(items, options) {
 
-    var out = "<ul>";
+var View ={
+    /**
+     * Simplify the loading of multiple template files
+     * @param target An array where add will store its results.
+     * @returns {{_after: _after, add: add, execute: execute}}
+     */
+    deferLoading : function(target){
 
-    for(var i=0, l=items.length; i<l; i++) {
-        out = out + "<li>" + options.fn(items[i]) + "</li>";
-    }
+        var deferred = [];
+        var loaded = 0;
+        var position = 0;
+        var promise = Q.defer();
 
-    return out + "</ul>";
-});
+        var ret = {
+            _after : function(){
+                if(++loaded < deferred.length)
+                    return;
 
-var View = function(viewFile, schema, callback){
-    fs.readFile(viewFile,function(err,source){
-        if(err){
-            console.log(err);
-            return;
+                promise.resolve();
+            },
+            /**
+             * Read in a file and store in the given target.  If data is defined, file will be compiled into a
+             * template and executed with data.  Otherwise the file as a string will be stored in target.
+             * @param file The full path to file to load
+             * @param data [optional] If data is sent, the file will be compiled into a template and executed with data
+             */
+            add : function(file, data){
+                var defer = Q.defer();
+                defer.promise.then(this._after);
+                deferred.push(defer);
+
+                (function(x){
+                    fs.readFile(file,function(err,src){
+                        if(err){
+                            console.log(err);
+                            return defer.reject();
+                        }
+
+                        if(data){
+                            var tpl = Handlebars.compile(src+"");
+                            target[x] = new Handlebars.SafeString(tpl(data));
+                        }else
+                            target[x] = src+"";
+
+                        defer.resolve();
+                    });
+                })(position++)
+            },
+            execute : function(){
+                return promise.promise;
+            }
+        };
+        return ret;
+    },
+    create : function(schema, route, callback){
+        this._doView(schema,'Create '+schema.getName(),'create',"/"+route+"/create",callback);
+    },
+    read : function(schema, route, callback){
+        this._doView(schema,'View '+schema.getName(),'read',null,callback);
+    },
+    update : function(schema, route, callback){
+        this._doView(schema,'Update '+schema.getName(),'update',"/"+route+"/update",callback);
+    },
+    delete : function(schema, route, callback){
+        this._doView(schema,'Delete '+schema.getName(),'delete',"/"+route+"/delete",callback);
+    },
+    success : function(schema,route,callback){
+        console.log('about to render success');
+        this._doView(schema,'Success','success',"/"+route+"/success",callback);
+    },
+    failure : function(schema,route,callback){
+        this._doView(schema,'Failure','failure',"/"+route+"/failure",callback);
+    },
+    _getTplName : function(type){
+        switch(type){
+            case 'create': return 'create.handlebars';
+            case 'read'  : return 'read.handlebars';
+            case 'update': return 'update.handlebars';
+            case 'delete': return 'delete.handlebars';
+            case 'success': return 'success.handlebars';
+            case 'failure': return 'failure.handlebars';
         }
-        var template = Handlebars.compile(source+"");
+    },
+    _doView : function(schema,title,type,action,callback){
+        //temp location to hold responses
+        var results = [];
+
+        //what will be sent to template
         var context = {
-            viewElements : []
+            head : null, foot : null, nav : null,
+            title : title,
+            viewElements : [],
+            action : action,
+            values : {}
         };
 
-        schema.each(function(field){
-            context.viewElements.push({view:new Handlebars.SafeString(field.getView().serialize())});
+        //load everything
+        var loader = new View.deferLoading(results);
+
+        //the main template
+        loader.add(__dirname+"/views/crud/"+this._getTplName(type));
+
+        //supporting templates
+        loader.add(__dirname+"/views/head.handlebars");
+        loader.add(__dirname+"/views/foot.handlebars");
+        loader.add(__dirname+"/views/nav.handlebars");
+
+
+
+        //the schema view templates
+        if(type !== 'success' && type !== 'failure'){
+            schema.each(function(field,idx){
+                var data = field.getView().serialize();
+                loader.add(__dirname+"/views/fields/"+data.views[type],data);
+            });
+        }
+
+
+        loader.execute().then(function(){
+            var template = Handlebars.compile(results.shift());
+
+            context.head = new Handlebars.SafeString(results.shift());
+            context.foot = new Handlebars.SafeString(results.shift());
+            context.nav  = new Handlebars.SafeString(results.shift());
+
+            //these were compiled template result and therefore are do not need escaped
+            context.viewElements = results.slice(0);
+
+            callback(template(context).replace(/\[\[/g,"{{").replace(/\]\]/g,"}}"));
         });
-
-        var html = template(context);
-        callback(html);
-    })
+    }
 };
-
 module.exports = View;
